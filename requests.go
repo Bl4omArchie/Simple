31d67d549a3a5f400bd6606b247bf2750bfc1813
Bl4omArchie <grlch.alexandre@proton.me>
2025-09-02 18:20:02 +0200

    add DownloadDocument()

--- requests.go
+++ requests.go
@@ -14,8 +14,10 @@ import (
 )
 
 
+// Get the body of a webpage.
+// Give the url and it will return the html body
 func GetContent(url string, client *http.Client) ([]byte, error) {
-	body, err := FetchBody(url, client)
+	body, err := fetchBody(url, client)
 	if err != nil {
 		return nil, err
 	}
@@ -24,8 +26,10 @@ func GetContent(url string, client *http.Client) ([]byte, error) {
 	return io.ReadAll(body)
 }
 
+// Get the body and parse it.
+// The package net/html allow to parse a html body into nodes to easily retrieve every html tags
 func GetParsedContent(url string, client *http.Client) (*html.Node, error) {
-	body, err := FetchBody(url, client)
+	body, err := fetchBody(url, client)
 	if err != nil {
 		return nil, err
 	}
@@ -34,7 +38,8 @@ func GetParsedContent(url string, client *http.Client) (*html.Node, error) {
 	return html.Parse(body)
 }
 
-func FetchBody(url string, client *http.Client) (io.ReadCloser, error) {
+// Core function to get a webpage body 
+func fetchBody(url string, client *http.Client) (io.ReadCloser, error) {
 	resp, err := client.Get(url)
 	if err != nil {
 		return nil, err
@@ -48,17 +53,16 @@ func FetchBody(url string, client *http.Client) (io.ReadCloser, error) {
 	return resp.Body, nil
 }
 
+// http client
+// Default config
 func HttpClient() *http.Client {
 	return &http.Client{Timeout: 30 * time.Second}
 }
 
-func OnionClient(socksProxy *string) (*http.Client, error) {
-	if socksProxy == nil {
-        defaultProxy := "127.0.0.1:9050"
-        socksProxy = &defaultProxy
-	}
-
-    dialer, err := proxy.SOCKS5("tcp", *socksProxy, nil, proxy.Direct)
+// onion client
+// Default config
+func OnionClient() (*http.Client, error) {
+    dialer, err := proxy.SOCKS5("tcp", "127.0.0.1:9050", nil, proxy.Direct)
     if err != nil {
         return nil, err
     }
@@ -76,19 +80,47 @@ func OnionClient(socksProxy *string) (*http.Client, error) {
 	return client, nil
 }
 
-func DownloadDocumentReturnHash(url string, filePath string, client *http.Client) (string, error) {
-	body, err := FetchBody(url, client)
+// download a document
+// give the url, the filepath where you want to store the document and the client
+func DownloadDocument(url string, filePath string, client *http.Client) error {
+	body, err := fetchBody(url, client)
 	if err != nil {
-		return "", err
+		return fmt.Errorf("failed to fetch body: %w", err)
 	}
+	defer body.Close()
 
-	if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
-		return "", err
+	if err := os.MkdirAll(filepath.Dir(filePath), 0755); err != nil {
+		return fmt.Errorf("failed to create directories: %w", err)
 	}
 
 	file, err := os.Create(filePath)
 	if err != nil {
-		return "", err
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer file.Close()
+
+	if _, err := io.Copy(file, body); err != nil {
+		return fmt.Errorf("failed to write file: %w", err)
+	}
+
+	return nil
+}
+
+// download a document and return its hash value 
+func DownloadDocumentReturnHash(url string, filePath string, client *http.Client) (string, error) {
+	body, err := fetchBody(url, client)
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch body: %w", err)
+	}
+	defer body.Close()
+
+	if err := os.MkdirAll(filepath.Dir(filePath), 0755); err != nil {
+		return "", fmt.Errorf("failed to create directories: %w", err)
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to create file: %w", err)
 	}
 	defer file.Close()
 
@@ -96,9 +128,8 @@ func DownloadDocumentReturnHash(url string, filePath string, client *http.Client
 	writer := io.MultiWriter(file, hasher)
 
 	if _, err := io.Copy(writer, body); err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to write file and compute hash: %w", err)
 	}
 
 	return fmt.Sprintf("%x", hasher.Sum(nil)), nil
 }
-
