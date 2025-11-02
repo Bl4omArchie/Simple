3eb4e31d45b2e5ef5ce89e438d74a044a75d973a
Bl4omArchie <grlch.alexandre@proton.me>
2025-11-02 17:03:11 +0100

    v1.0.2, se changelog 02/11/25

--- tests/requests_test.go
+++ tests/requests_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"os"
 	"context"
 	"testing"
 	"net/http"
@@ -9,20 +10,61 @@ import (
 	"github.com/Bl4omArchie/simple"
 )
 
-
-// MOCK for default
+// MOCK	for GetContent
 func TestGetContentMock(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("hello world"))	}))
 	defer ts.Close()
 
-	var ctx context.Context
+	var ctx context.Context = context.Background()
 
-	client := simple.HttpClient()
-	content, err := simple.GetContent(ctx, ts.URL, client)
+	content, err := simple.GetContent(ctx, ts.URL, simple.HttpClient(), nil)
 	if err != nil {
 		t.Fatalf("GetContent failed: %v", err)
 	}
 	if string(content) != "hello world" {
 		t.Errorf("Unexpected content: %s", string(content))
+	}
+}
+
+// MOCK	for GetContent with custom request
+func TestGetContentCustomReqMock(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("hello world"))	}))
+	defer ts.Close()
+
+	var ctx context.Context = context.Background()
+
+	content, err := simple.GetContent(ctx, ts.URL, simple.HttpClient(), func(req *http.Request) error {
+		req.Header.Set("User-Agent", "Go-http-client/1.1")
+		req.Header.Set("Accept", "*/*")
+		return nil
+	})
+	if err != nil {
+		t.Fatalf("GetContent with custom req failed: %v", err)
+	}
+	if string(content) != "hello world" {
+		t.Errorf("Unexpected content: %s", string(content))
+	}
+}
+
+// MOCK for DownloadDocument
+func TestDownloadDocumentMock(t *testing.T) {
+	var ctx context.Context = context.Background()
+	var url string = "https://freetestdata.com/document-files/pdf/"
+
+	var folder string = "doc/"
+	var path string = "doc/test.pdf"
+
+	err := simple.DownloadDocument(ctx, url, path, simple.HttpClient(), nil)
+	if err != nil {
+		t.Fatalf("GetContent failed: %v", err)
+	}
+
+	if _, err := os.Stat(path); err != nil {
+		t.Fatalf("couldn't find downloaded document")
+	}
+
+	err = os.RemoveAll(folder)
+	if err != nil {
+		t.Fatalf("couldn't delete file")
 	}
 }
